b9a61522a032480414d60352ca8293ab31513d66
maxtoor <794382+maxtoor@users.noreply.github.com>
2026-03-03 09:32:34 +0100

    Fix login CSRF issue and refine quick help modal behavior

--- agile/views.py
+++ agile/views.py
@@ -11,6 +11,7 @@ from rest_framework import status, viewsets
 from rest_framework.authtoken.models import Token
 from rest_framework.decorators import action
 from rest_framework.exceptions import ValidationError
+from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -225,6 +226,7 @@ def notify_change_request_submitted(*, change_request: ChangeRequest) -> bool:
 
 
 class LoginView(APIView):
+    authentication_classes = [TokenAuthentication]
     permission_classes = [AllowAny]
 
     def post(self, request):
